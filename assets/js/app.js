386a3803853718bc02e6f3faeed6eef437f858fb
Dan Reeves <heydanreeves@gmail.com>
2016-10-05 21:31:00 +0100

    linting and stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+/* global document */
 import Vue from 'vue';
 import Vuex from 'vuex';
 
@@ -9,60 +10,60 @@ const state = {
 };
 
 const mutations = {
-    eval (state) {
+    eval (st) {
         try {
-            state.value = (`${(new Function(`return ${state.calc}`))()}`).trim();
+            st.value = (`${(new Function(`return ${st.calc}`))()}`).trim(); // eslint-disable-line no-new-func
         } catch (e) {}
         // I don't care about failures. Don't update the view
     },
-    input (state, evt) {
+    input (st, evt) {
         const val = evt.target.value || '';
-        state.calc = (state.calc + `${val}`).trim();
-        mutations.eval(state);
-        Vue.nextTick(function () {
+        st.calc = (`${st.calc}${val}`).trim();
+        mutations.eval(st);
+        Vue.nextTick(() => {
             const calcEl = document.querySelector('.calc .scroll');
             calcEl.scrollLeft = calcEl.scrollWidth * 2;
         });
     },
-    clear (state) {
-        state.value = '';
-        state.calc = '';
-    }
+    clear (st) {
+        st.value = '';
+        st.calc = '';
+    },
 };
 
 const calc = {
     render (h) {
         return (
-        <div class="calculator">
-            <div class="value">
-                <div class="scroll">
-                    <div>{ this.value }</div>
+            <div class="calculator">
+                <div class="value">
+                    <div class="scroll">
+                        <div>{ this.value }</div>
+                    </div>
+                </div>
+                <div class="calc">
+                    <div class="scroll">
+                        <div>{ this.calcDisplay }</div>
+                    </div>
+                </div>
+                <div class="pad">
+                    <button value="7" on-click={this.input}>7</button>
+                    <button value="8" on-click={this.input}>8</button>
+                    <button value="9" on-click={this.input}>9</button>
+                    <button value="*" on-click={this.input}>×</button>
+                    <button value="4" on-click={this.input}>4</button>
+                    <button value="5" on-click={this.input}>5</button>
+                    <button value="6" on-click={this.input}>6</button>
+                    <button value="/" on-click={this.input}>÷</button>
+                    <button value="1" on-click={this.input}>1</button>
+                    <button value="2" on-click={this.input}>2</button>
+                    <button value="3" on-click={this.input}>3</button>
+                    <button value="+" on-click={this.input}>+</button>
+                    <button value="0" on-click={this.input}>0</button>
+                    <button value="." on-click={this.input}>.</button>
+                    <button on-click={this.clear}>C</button>
+                    <button value="-" on-click={this.input}>−</button>
                 </div>
             </div>
-            <div class="calc">
-                <div class="scroll">
-                    <div>{ this.calcDisplay }</div>
-                </div>
-            </div>
-            <div class="pad">
-                <button value="7" on-click={this.input}>7</button>
-                <button value="8" on-click={this.input}>8</button>
-                <button value="9" on-click={this.input}>9</button>
-                <button value="*" on-click={this.input}>×</button>
-                <button value="4" on-click={this.input}>4</button>
-                <button value="5" on-click={this.input}>5</button>
-                <button value="6" on-click={this.input}>6</button>
-                <button value="/" on-click={this.input}>÷</button>
-                <button value="1" on-click={this.input}>1</button>
-                <button value="2" on-click={this.input}>2</button>
-                <button value="3" on-click={this.input}>3</button>
-                <button value="+" on-click={this.input}>+</button>
-                <button value="0" on-click={this.input}>0</button>
-                <button value="." on-click={this.input}>.</button>
-                <button on-click={this.clear}>C</button>
-                <button value="-" on-click={this.input}>−</button>
-            </div>
-        </div>
         );
     },
     computed: {
@@ -86,9 +87,9 @@ const calc = {
     ]),
 };
 
-const app = new Vue({
+new Vue({
     el: '#app',
-    render (h) { return h('calc'); },
+    render (h) { return <calc />; },
     components: {
         calc,
     },
